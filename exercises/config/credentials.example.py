14c8ec20df78e197b17e5a333ad3644465f14104
ffichtenmueller <frank.fichtenmueller@outlook.com>
2016-10-25 01:11:37 +0200

    updated the exercises folder

---
+++ exercises/config/credentials.example.py
@@ -0,0 +1,8 @@
+"""This is an example credentials file. Replace the keys/secrets in this file
+with your own info, then save that file as credentials.py.
+"""
+
+ACCESS_TOKEN_KEY = "replaceWithYourTwitterAppKey"
+ACCESS_TOKEN_SECRET = "replaceWithYourTwitterAppSecret"
+CONSUMER_KEY = "replaceWithYourTwitterAppKey"
+CONSUMER_SECRET = "replaceWithYourTwitterAppSecret"
